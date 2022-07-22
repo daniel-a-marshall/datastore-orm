4710d300a0c0771685cff87c0162245c7c529a17
Dan Marshall <daniel_a_marshall@whirlpool.com>
2022-07-22 13:32:27 -0400

    run build on prior commit

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -17,17 +17,17 @@ export declare type QueryOptions = {
 export declare type OrmTransaction<T> = {
     transaction: Transaction;
     create: (data: Omit<T, "_id">) => void;
-    get: (id: string | number) => Promise<T>;
-    query: (options?: QueryOptions) => Promise<T[]>;
-    update: (id: string | number, change: Partial<Omit<T, "_id">>) => void;
-    destroy: (id: string | number) => void;
+    get: (id: string | number, validate?: (entity: T) => void) => Promise<T>;
+    query: (options?: QueryOptions, validate?: (entities: T[]) => void) => Promise<T[]>;
+    update: (id: string | number, change: Partial<Omit<T, "_id">>, validate?: (entity: T) => void) => void;
+    destroy: (id: string | number, validate?: (entity: T) => void) => void;
 };
 export declare type OrmModel<T> = {
     create: (data: Omit<T, "_id">) => Promise<T>;
-    get: (id: string | number) => Promise<T>;
-    query: (options?: QueryOptions) => Promise<T[]>;
-    update: (id: string | number, change: Partial<Omit<T, "_id">>) => Promise<T>;
-    destroy: (id: string | number, validate: (entity: T) => void) => Promise<any>;
+    get: (id: string | number, validate?: (entity: T) => void) => Promise<T>;
+    query: (options?: QueryOptions, validate?: (entities: T[]) => void) => Promise<T[]>;
+    update: (id: string | number, change: Partial<Omit<T, "_id">>, validate?: (entity: T) => void) => Promise<T>;
+    destroy: (id: string | number, validate?: (entity: T) => void) => Promise<any>;
     transaction: (func: (transaction: OrmTransaction<T>) => void) => Promise<void>;
 };
 export default function createORM(options?: {
